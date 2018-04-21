4ef306c5015293317ece62a75df74ac2cb4a89cc
nesarares <nesa.rares@gmail.com>
2018-04-21 13:23:48 +0300

    Added login GUI

---
+++ DonationClient/src/main/java/donation/client/controllers/LoginController.java
@@ -0,0 +1,13 @@
+package donation.client.controllers;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
